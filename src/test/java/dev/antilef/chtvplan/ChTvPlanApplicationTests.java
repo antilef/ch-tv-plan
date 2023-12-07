ca6bcf59f744397cf2a07cd814166dff46043d46
Francisco Antilef <fi.antilef@gmail.com>
2023-12-07 19:18:28 -0300

    Comment unsed testSpringClass

--- src/test/java/dev/antilef/chtvplan/ChTvPlanApplicationTests.java
+++ src/test/java/dev/antilef/chtvplan/ChTvPlanApplicationTests.java
@@ -1,13 +1,13 @@
-package dev.antilef.chtvplan;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ChTvPlanApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package dev.antilef.chtvplan;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ChTvPlanApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}

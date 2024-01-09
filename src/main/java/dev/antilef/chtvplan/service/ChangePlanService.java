4f6f6f1803d2f71442caf097c15f88c132772589
Francisco Antilef <fi.antilef@gmail.com>
2024-01-09 16:54:01 -0300

    WIP:Append success filter to finish filter chain

--- src/main/java/dev/antilef/chtvplan/service/ChangePlanService.java
+++ src/main/java/dev/antilef/chtvplan/service/ChangePlanService.java
@@ -3,7 +3,6 @@ package dev.antilef.chtvplan.service;
 import dev.antilef.chtvplan.dto.ChangePlanOperationResult;
 import dev.antilef.chtvplan.dto.TryToChangePlanRequest;
 import dev.antilef.chtvplan.dto.TryChangePlanResponse;
-import dev.antilef.chtvplan.dto.CreateTicketChangePlanDTO;
 import dev.antilef.chtvplan.entity.ChangePlanResultFilter;
 import dev.antilef.chtvplan.entity.DestinyPlan;
 import dev.antilef.chtvplan.filters.FiltersRunner;
@@ -20,7 +19,7 @@ import java.util.Optional;
 
 public class ChangePlanService {
 
-    private Logger logger = LoggerFactory.getLogger(ChangePlanService.class);
+    private final Logger logger = LoggerFactory.getLogger(ChangePlanService.class);
 
     @Autowired
     private final DestinyPlanRepository destinyPlanRepository;
@@ -58,14 +57,14 @@ public class ChangePlanService {
             response.setResult(result.getStatus());
             return response;
         }
-        if (result.getStatus() != null && result.getStatus().equals(ChangePlanResult.SUCCESS)) {
-            operationResult = cambioPlanAutomatico(requestDTO, result);
-        }
-
-
         BigDecimal createdTicket = null;
-
-        if (!operationResult.isError()) {
+        if (result.getStatus() != null && result.getStatus().equals(ChangePlanResult.SUCCESS)) {
+            operationResult = changeAutomaticPlan(requestDTO, result);
+            if (operationResult.isError()) {
+                response.setResult(ChangePlanResult.ERROR);
+                response.setReason("One service is not available");
+                return response;
+            }
             createdTicket = crearTicket(requestDTO, result);
             response.setTicketNumber(createdTicket);
         }
@@ -74,8 +73,7 @@ public class ChangePlanService {
         response.setReason(result.getReason());
         response.setResult(result.getStatus());
 
-
-        //sendMailCambioPlanAutomatico(requestDTO, result, createdTicket);
+        sendMail(requestDTO, result, createdTicket);
 
 
         return response;
@@ -114,86 +112,15 @@ public class ChangePlanService {
 
         return result;
 
-
-
-//
-//        String isActiveChange = propertiesRepository.getProperty("cpo.activo");
-//        if (!isActiveChange.equals(Constants.CPO_ACTIVE_VALID)) {
-//            reason = propertiesRepository.getProperty(CPO_OK_MANUAL);
-//            result = new ChangePlanResultFilter(ChangePlanResult.MANUAL, reason, cbpID, fixedChargeOrigin, fixedChargeDestiny,
-//                    "CPO INACTIVO");
-//            return result;
-//        }
-
-
-//        reason = propertiesRepository.getProperty("cpo.OK_AUTOMATICO");
-//        result = new ChangePlanResultFilter(ChangePlanResult.SUCCESS, reason, cbpID, fixedChargeOrigin, fixedChargeDestiny,
-//                "AUTOMATICO");
     }
 
 
-    public void sendMailCambioPlanAutomatico(){
+    public void sendMail(TryToChangePlanRequest requestDTO, ChangePlanResultFilter result, BigDecimal createdTicket){
 
     }
 
-    public ChangePlanOperationResult cambioPlanAutomatico(TryToChangePlanRequest request, ChangePlanResultFilter filterResult) {
+    public ChangePlanOperationResult changeAutomaticPlan(TryToChangePlanRequest request, ChangePlanResultFilter filterResult) {
+        logger.info("Calling to webServices to change plan");
         return new ChangePlanOperationResult();
-//        logger.info("Inicia Cambio de plan automatico ReplaceOffer");
-//        response.put(RESPUESTA, MSG_NOK);
-//        try {
-//            Cd105CpoPlanDestino planDestino = destinyPlanDAO.findBy(request.getRequestedPlan()).get(0);
-//            String cBPID = filterResult.getCpbId();
-//            String mSISDN = request.getMsisdn();
-//            String targetOffer = planDestino.getPlanId();
-//
-//            ReplaceOfferResponse replaceOffer = wsGet.replaceOfferWS(cBPID, mSISDN, targetOffer);
-//            logger.info("Respuesta ReplaceOffer");
-//            if (logger.isInfoEnabled() && replaceOffer != null) {
-//                logger.info(replaceOffer.toString());
-//            }
-//            String offerID = null;
-//            if (replaceOffer != null && replaceOffer.getOfferInfo() != null) {
-//                offerID = replaceOffer.getOfferInfo().getOfferID();
-//            }
-//
-//            if (offerID != null && !offerID.isEmpty() && replaceOffer.getOfferInfo() != null) {
-//                offerID = replaceOffer.getOfferInfo().getOfferID();
-//                String descripcion = replaceOffer.getOfferInfo().getDescripcion();
-//                String offerPrice = replaceOffer.getOfferInfo().getOfferPrice();
-//                String mainBOPlan = replaceOffer.getOfferInfo().getMainBOPlan();
-//                logger.info("offerID {} ", offerID);
-//                logger.info("descripcion {} ", descripcion);
-//                logger.info("offerPrice {}", offerPrice);
-//                logger.info("mainBOPlan {} ", mainBOPlan);
-//                response.put(RESPUESTA, MSG_OK);
-//                response.put(MENSAJE, "el cambio HA IDO BIEN !!!");
-//                logger.info("el cambio HA IDO BIEN !!!");
-//            } else {
-//                if (replaceOffer != null && replaceOffer.getResponseFailure() != null) {
-//                    logger.info("getResponseFailure {}", replaceOffer.getResponseFailure());
-//
-//                }
-//                String errorDescription = null;
-//                if (replaceOffer != null && replaceOffer.getResponseFailure() != null) {
-//                    errorDescription = replaceOffer.getResponseFailure().getErrorDescription();
-//                }
-//                logger.info(errorDescription);
-//                if (errorDescription != null && errorDescription.equals("PRODUCT HAS A PENDING ORDER IN OMS DB")) {
-//                    logger.info("Cambio de plan en curso ReplaceOffer");
-//                    response.put(ERROR, propertieDAO.getPropertie(SUCVIRDASH, WEB, "cpo.CHANGE_PLAN_ENCURSO"));
-//                } else {
-//                    logger.info("Error al hacer el cambio de plan ReplaceOffer");
-//                    response.put(ERROR, propertieDAO.getPropertie(SUCVIRDASH, WEB, "cpo.CHANGE_PLAN_ERROR"));
-//                }
-//                response.put(MENSAJE, "el cambio AQUI NO HA IDO BIEN " + errorDescription);
-//                response.put("errorDescription", errorDescription);
-//
-//            }
-//        } catch (Exception e) {
-//            logger.info("Exception ReplaceOffer {} ", e.getMessage());
-//            logger.error("Error [cambioPlanAutomatico]: {} ", e.getMessage());
-//        }
-//        logger.info("Fin Cambio de plan automatico ReplaceOffer");
-//        return response;
     }
 }
